d5be8ffe14c9ce8f48d6ec09e642214fcf124312
Jaren Flaker <jarenflaker@gmail.com>
2019-09-21 22:24:02 -0600

    fixed password

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'development'){
     module.exports = {mongoURI:
-        "mongodb+srv://Jaren:6428jf@taskjot-o4a8p.mongodb.net/test?retryWrites=true&w=majority"
+        "mongodb+srv://Jaren:6428Jaren@taskjot-o4a8p.mongodb.net/test?retryWrites=true&w=majority"
      }
 }
 else{
