b68f73fcfe9d504527ec71edd7dbba6437a055f3
Jaren Flaker <jarenflaker@gmail.com>
2019-09-21 22:42:24 -0600

    another try at fixing URI

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV){
     module.exports = {mongoURI:
         "mongodb+srv://Jaren:6428Jaren@taskjot-o4a8p.mongodb.net/test?retryWrites=true&w=majority"
      }
